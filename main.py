b1f7cddc37c0499620e2f8bd4a8013bef7c05ed2
Gamemanuel <themathnight@gmail.com>
2025-03-24 10:51:41 -0700

    started file structure

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
-#!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor
-from pybricks.parameters import Port
+from pybricks.ev3devices import Motor, UltrasonicSensor
+from pybricks.parameters import Port, Direction
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+from pybricks.media.ev3dev import SoundFile
+from random import randint
 
-# Create your objects here
-
-# Initialize the EV3 Brick.
+# Initialize the EV3 brick.
 ev3 = EV3Brick()
 
-# Initialize a motor at port B.
-test_motor = Motor(Port.B)
+left_motor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+right_motor = Motor(Port.D, Direction.COUNTERCLOCKWISE)
 
-# Write your program here
+# constants 
+WHEEL_DIAMETER = 45 # this is in mm
+AXLE_TRACK = 150 # this is in mm
 
-# Play a sound.
-ev3.speaker.beep()
+# DriveBase(left_motor, right_motor, wheel_diameter, axle_track)
+# left_motor (Motor) – The motor that drives the left wheel.
+# right_motor (Motor) – The motor that drives the right wheel.
+# wheel_diameter (dimension: mm) – Diameter of the wheels.
+# axle_track (dimension: mm) – Distance between the points where both wheels touch the ground.
 
-# Run the motor up to 500 degrees per second. To a target angle of 90 degrees.
-test_motor.run_target(500, 90)
+robot = DriveBase(left_motor, right_motor, WHEEL_DIAMETER, AXLE_TRACK)
 
-my_motor.run_angle(500, 90, Stop.HOLD, False)
+# settings(straight_speed, straight_acceleration, turn_rate, turn_acceleration)
+# straight_speed (speed: mm/s) – Speed of the robot during straight().
+# straight_acceleration (linear acceleration: mm/s/s) – Acceleration and deceleration of the robot at the start and end of straight().
+# turn_rate (rotational speed: deg/s) – Turn rate of the robot during turn().
+# turn_acceleration (rotational acceleration: deg/s/s) – Angular acceleration and deceleration of the robot at the start and end of turn().
 
-# Play another beep sound.
-ev3.speaker.beep(frequency=1000, duration=500)
+robot.settings(1,1,1,1) # sets the onfiguration of the motor speeds
 
-# https://pybricks.com/ev3-micropython/index.html
-# https://docs.pybricks.com/en/latest/iodevices/xboxcontroller.html
